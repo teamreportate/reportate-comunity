2ae80c01b43936a402166da9fa0e683a957a4d31
Ruth Vargas <ruth.vargas@srlpunto.com>
2020-04-06 13:19:32 -0400

    update report grapic

--- source/back-office/back-office-ui/src/app/dashboards/principal/principal.component.ts
+++ source/back-office/back-office-ui/src/app/dashboards/principal/principal.component.ts
@@ -1,18 +1,17 @@
-import {AfterViewInit, ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import {ChartEvent, ChartType} from 'ng-chartist/dist/chartist.component';
-import {MatDialog} from '@angular/material';
-import {DashboardService} from '../../core/services/http-services/dashboard.service';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import * as moment from 'moment';
-import {ClicComponent} from '../../core/utils/clic-component';
-import {MediaMatcher} from '@angular/cdk/layout';
-import {Router} from '@angular/router';
-import {NotifierService} from 'angular-notifier';
-import {Page} from '../../core/utils/paginator/page';
-import {CustomOptions} from '../../core/models/dto/custom-options';
-import {BlockUI, NgBlockUI} from 'ng-block-ui';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import echarts from '../../../assets/js/echarts.min.js';
+
+import { DashboardService } from '../../core/services/http-services/dashboard.service';
+import { ClicComponent } from '../../core/utils/clic-component';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { NotifierService } from 'angular-notifier';
+import { Page } from '../../core/utils/paginator/page';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { CustomOptions } from '../../core/models/dto/custom-options';
 
 @Component({
   selector: 'app-principal',
@@ -25,40 +24,19 @@ export class PrincipalComponent extends ClicComponent implements OnInit, AfterVi
   public form: FormGroup;
   mostrar: boolean;
 
+  from = new Date();
+  to = new Date();
 
   echarts = echarts;
   myChart: any;
 
-  option = {
-    legend: {},
-    tooltip: {},
-    dataset: {
-        dimensions: ['registrado', 'Alto', 'Medio', 'Bajo'],
-        source: [
-            {registrado: '01/04/2020', 'Alto': 43, 'Medio': 85, 'Bajo': 93},
-            {registrado: '02/04/2020', 'Alto': 83, 'Medio': 73, 'Bajo': 55},
-            {registrado: '03/04/2020', 'Alto': 86, 'Medio': 65, 'Bajo': 82},
-            {registrado: '04/04/2020', 'Alto': 52, 'Medio': 53, 'Bajo': 39},
-            {registrado: '05/04/2020', 'Alto': 72, 'Medio': 13, 'Bajo': 19},
-            {registrado: '05/04/2020', 'Alto': 32, 'Medio': 10, 'Bajo': 29},
-            {registrado: '07/04/2020', 'Alto': 42, 'Medio': 55, 'Bajo': 30},
-            {registrado: '08/04/2020', 'Alto': 52, 'Medio': 53, 'Bajo': 39},
-            {registrado: '09/04/2020', 'Alto': 72, 'Medio': 53, 'Bajo': 39},
-            {registrado: '10/04/2020', 'Alto': 72, 'Medio': 12, 'Bajo': 30}
-        ]
-    },
-    xAxis: {type: 'category'},
-    yAxis: {},
-    series: [
-        {type: 'bar'},
-        {type: 'bar'},
-        {type: 'bar'}
-    ]
-};
+  data = [];
+
+  option: any;
 
 
-  constructor(private dialog: MatDialog, private dashboardService: DashboardService,
-              private changeDetector: ChangeDetectorRef, private media: MediaMatcher, private router: Router, private notifier: NotifierService) {
+  constructor(private dialog: MatDialog, private service: DashboardService,
+    private changeDetector: ChangeDetectorRef, private media: MediaMatcher, private router: Router, private notifier: NotifierService) {
     super();
     this.mostrar = false;
   }
@@ -67,14 +45,66 @@ export class PrincipalComponent extends ClicComponent implements OnInit, AfterVi
 
   ngOnInit() {
     this.initialListener(this.changeDetector, this.media);
-
-    this.myChart = this.echarts.init(document.getElementById('main'));
-    this. myChart.setOption(this.option);
+    this.initForm();
   }
+
+  initForm() {
+    this.from = new Date(this.from.setDate(this.from.getDate() - 3));
+    this.form = new FormGroup({
+      from: new FormControl(this.from, Validators.compose([Validators.required])),
+      to: new FormControl(this.to, Validators.compose([Validators.required])),
+    }, {
+    });
+  }
+
+  getByValorationRequest() {
+    if (this.form.valid) {
+      const formValue = this.form.value;
+      const from = formValue.from.getDate() + '%2F' + (formValue.from.getMonth() + 1) + '%2F' + formValue.from.getFullYear();
+      const to = formValue.to.getDate() + '%2F' + (formValue.to.getMonth() + 1) + '%2F' + formValue.to.getFullYear();
+
+      this.service.byValorationRequest(from, to).subscribe(response => {
+        this.data = response.body.datas;
+        this.draw(this.data);
+
+        this.blockUI.stop();
+      }, error => {
+        this.blockUI.stop();
+        if (error) this.notifierError(error);
+      });
+    }
+  }
+
+  draw(data: any[]) {
+    this.option = {
+      legend: {},
+      tooltip: {},
+      dataset: {
+        dimensions: ['registrado', 'alto', 'medio', 'bajo'],
+        source: data
+      },
+      xAxis: { type: 'category' },
+      yAxis: {},
+      series: [
+        { type: 'bar' },
+        { type: 'bar' },
+        { type: 'bar' }
+      ]
+    };
+    this.myChart = this.echarts.init(document.getElementById('main'));
+    this.myChart.setOption(this.option);
+  }
+
+
+
+
+
+
+
 
   notifierError(error: any, type?: string) {
     if (error && error.error) {
-      const customOptions: CustomOptions = {type: type ? type : 'error', tile: error.error.title, message: error.error.detail, template: this.customNotificationTmpl};
+      const customOptions: CustomOptions = { type: type ? type : 'error', tile: error.error.title, message: error.error.detail, template: this.customNotificationTmpl };
       this.notifier.show(customOptions);
     }
   }
