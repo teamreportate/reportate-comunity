b6607246924566288e5de215065b46ac547c9efc
Marcos Yugar <yugarmarcos@gmail.com>
2020-04-12 19:33:20 -0400

    fix tacometro

--- source/backend/model/src/main/java/bo/com/reportate/model/dto/response/TotalResponse.java
+++ source/backend/model/src/main/java/bo/com/reportate/model/dto/response/TotalResponse.java
@@ -3,24 +3,21 @@ package bo.com.reportate.model.dto.response;
 import java.io.Serializable;
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter @NoArgsConstructor @Data
-public class GraficoDto implements Serializable{
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Data
+public class TotalResponse implements Serializable{
 	
 	private static final long serialVersionUID = 8896814117952763756L;
-	private String nombreGrafico;
-	private Integer cantidadGrafico;
-	private Integer cantidadMaximaGrafico;
-	
-	public GraficoDto(String nombreGrafico, Integer cantidadGrafico) {
-		super();
-		this.nombreGrafico = nombreGrafico;
-		this.cantidadGrafico = cantidadGrafico;
-	}
-	
+	private Integer sospechosos;
+	private Integer descartados;
+	private Integer confirmados;
+	private Integer recuperados;
+	private Integer fallecidos;
+	private Integer total;
 	
 }
